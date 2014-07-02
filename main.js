4aeba35230195bd1f7c4fbca823a22511927aafe
Teri <tealynsea@hotmail.com>
2014-07-02 17:37:18 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,76 @@
+$(document).on('ready', function() {
+  
+//problem 1//
+/*function getInfo(name, age) {
+
+
+	alert(age);
+}
+
+
+getInfo(name = "Luisa", age = 25);*/
+
+//Just practice for calculating
+/*function totalLetters(w1, w2, w3) {
+	
+	var total = w1.length + w2.length + w3.length;
+
+	return total;
+	
+
+}
+
+console.log(totalLetters('javascript', 'is','awesome'));*/
+
+/*function totalLetters(words) {
+
+		var count = 0;
+		for (var i=0; i<words.length; i++){
+			count += words[i].length;
+
+		}
+			return count;
+	}
+
+
+
+ 
+console.log(totalLetters(['javascript', 'is', 'awesome']));
+console.log(totalLetters(['what', 'happened', 'to', 'my', 'function']));*/
+
+
+/*function keyValue(city) {
+
+	var obj = {
+		city: city,
+		 
+	};
+	return obj;
+}
+	
+console.log(keyValue('Denver'));*/
+
+
+function negativeIndex(index) {
+
+	  
+
+
+	}
+		
+
+
+
+console.log(negativeIndex(['a', 'b', 'c', 'd', 'e'], -2));
+
+
+
+ 
+
+
+
+});
+
+ 
+
+ 
